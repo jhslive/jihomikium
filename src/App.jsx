a6ba5db74939e58c1603dbf13973712ff2ca282c
jhslive <111948596+jhslive@users.noreply.github.com>
2023-02-10 16:15:50 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [isLong, setIsLong] = useState(true); //eslint-disable-line no-unused-vars
   const [leverage, setLeverage] = useState(1); //eslint-disable-line no-unused-vars
   const [result, setResult] = useState(
-        (((((bidPrice*maxPrice)-((askPrice*0.015)+(bidPrice*0.015)+((bidPrice * maxPrice)*0.003)))-(askPrice*maxPrice))/(1000*100))*100).toFixed(2)
+        (((((bidPrice*maxPrice)-((askPrice*(0.015*100))+(bidPrice*(0.015*100))+((bidPrice * maxPrice)*(0.003*100))))-(askPrice*maxPrice))/(1000*100))*100).toFixed(2)
   );
   const [tangu, setTangu] = useState(false);
 
